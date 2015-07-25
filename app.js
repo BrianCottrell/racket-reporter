ef5fc85baf9e691d14d3b3494c18824f071ffff2
Brian Cottrell <bcottrell@ezoic.com>
2015-07-25 16:36:12 -0700

    Added swing detection

--- app.js
+++ app.js
@@ -2,48 +2,107 @@
 /* by Brian Cottrell  */
 /* 07-25-2015         */
 
+//Create a basic express app
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
-
+//Add Myo armband library
 var Myo = require('myo');
-
-var count = 0;
-
+//Track the gestures detected
+var swing = {
+    swingType: 'none',
+    strength: 'none',
+    direction: 'none',
+    count: 0
+};
 //Start talking with Myo Connect
 Myo.connect();
+
+Myo.on('imu', function(data){
+    if(data.gyroscope.z > 200){
+        swing.swingType = 'underhand';
+        swing.direction = 'backhand';
+        if(swing.strength == 'none'){
+            swing.strength = 'slow';
+        }
+        if(data.gyroscope.z > 400){
+            if(swing.strength != 'fast'){
+                swing.strength = 'medium';
+            }
+            if(data.gyroscope.z > 500){
+                swing.strength = 'fast';
+            }
+        }
+        swing.count++;
+        console.log(swing);
+    }
+    else if(data.gyroscope.z < -200){
+        swing.swingType = 'underhand';
+        swing.direction = 'forehand';
+        if(swing.strength == 'none'){
+            swing.strength = 'slow';
+        }
+        if(data.gyroscope.z < -400){
+            if(swing.strength != 'fast'){
+                swing.strength = 'medium';
+            }
+            if(data.gyroscope.z < -500){
+                swing.strength = 'fast';
+            }
+        }
+        swing.count++;
+        console.log(swing);
+    }
+    else if(data.gyroscope.y > 300){
+        swing.swingType = 'overhand';
+        swing.direction = 'forehand';
+        if(swing.strength == 'none'){
+            swing.strength = 'slow';
+        }
+        if(data.gyroscope.y > 400){
+            if(swing.strength != 'fast'){
+                swing.strength = 'medium';
+            }
+            if(data.gyroscope.y > 500){
+                swing.strength = 'fast';
+            }
+        }
+        swing.count++;
+        console.log(swing);
+    }
+});
 
 Myo.on('fist', function(){
     console.log('fist gesture detected');
     this.vibrate();
-    count++;
 });
 
 Myo.on('fingers_spread', function(){
     console.log('finger spread gesture detected');
     this.vibrate();
-    count++;
 });
 
 Myo.on('wave_in', function(){
     console.log('wave in gesture detected');
     this.vibrate();
-    count++;
 });
 
 Myo.on('wave_out', function(){
     console.log('wave out gesture detected');
     this.vibrate();
-    count++;
 });
 
 app.get('/racket_report', function(req, res){
-    res.status(200).send(count+'');
+    res.status(200).send(swing);
+    swing = {
+        swingType: 'none',
+        strength: 'none',
+        direction: 'none',
+    };
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 //Run server
 http.listen(port, function() {
-  console.log('listening on *:8080');
+  console.log('listening on 80');
 });
